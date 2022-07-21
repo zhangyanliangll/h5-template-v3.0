945c761b1ba2d0b8fba788944ea3dda97f7d3226
张燕良 <zhangyanliang@kfang.com>
2022-07-21 10:46:21 +0800

    chore: init

--- src/utils/tools/index.ts
+++ src/utils/tools/index.ts
@@ -10,7 +10,8 @@ export const cleanCookie = (): void => {
   const keys = document.cookie.match(/[^ =;]+(?=\=)/g)
   console.log('需要删除的cookie名字：' + keys)
   if (keys) {
-    for (let i = keys.length; i--; )
+    // eslint-disable-next-line prettier/prettier
+    for (let i = keys.length; i--;)
       document.cookie =
         keys[i] + '=0; expire=' + date.toUTCString() + '; path=/'
   }
