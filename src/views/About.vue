c8d5b262e11186d6b517e3ed148cf5c0b95782ea
zhangyanliangll <445057661@qq.com>
2023-04-25 17:28:04 +0800

    fix: init

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,23 @@
 <template>
-  <div class="about"></div>
+  <h1>姓名：{{ name }}</h1>
+  <h1>年龄：{{ age }}</h1>
+  <h2>职业：{{ job.occupation }}<br />薪资：{{ job.salary }}</h2>
+  <h3>爱好：{{ hobby[0] }},{{ hobby[1] }},{{ hobby[2] }}</h3>
+  <button @click="say">修改</button>
 </template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+const count = ref(0)
+
+const obj = ref({
+  count: 0,
+})
+
+console.log(obj)
+console.log(count)
+
+const addCount = () => {
+  obj.value.count++
+}
+</script>
